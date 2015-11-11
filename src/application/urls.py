79aae15281c17a7efa272200b241431b0341ec4b
artem <artem.rudoj@frtk.ru>
2015-11-11 14:55:32 +0300

    added bootstrap forms

--- src/application/urls.py
+++ src/application/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from decriptor.views import HomePage
+from decriptor.views import ResultPage
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomePage.as_view(), name='home'),
+    url(r'^decripted/$', ResultPage.as_view(), name='result'),
 ]
--- src/decriptor/views.py
+++ src/decriptor/views.py
@@ -2,4 +2,7 @@ from django.views.generic import TemplateView
 
 
 class HomePage(TemplateView):
-    template_name = "base.html"
+    template_name = "input.html"
+
+class ResultPage(TemplateView):
+    template_name = "result.html"
